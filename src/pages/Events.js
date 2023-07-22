ef6c2a36381565056a2d8393f95c3aa5d33b73a7
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-22 16:35:18 -0500

    Removed course instruction comments from App.js. Moved loader() code to Events.js, and imported into App.js. Refactored response into a return object with ‘return response’.

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,14 +2,24 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <div>
-      <EventsList events={events} />}
+      <EventsList events={events} />
     </div>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //... later
+  } else {
+    return response;
+  }
+}
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, 
+  //useNavigation 
+} 
+  from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
+
+  //const navigation = useNavigation();
+
     return (
       <div>
         <MainNavigation />
         <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
           <Outlet />
         </main>
       </div>
