77db45aedd84a9a7ba61cbc1f769a74a5882b8fe
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-23 15:09:49 -0500

    Import Link to EventsList.js. Change anchors on that page to Link to event.id. Added loader func to EventDetail.js. Added loader prop to :eventiD route. Called useLoaderData in EventDetail page, and passed that loader data to EventItem event prop in EventDetail page.

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,16 +1,32 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
 
-  const params = useParams();
+  const data = useLoaderData();
 
   return (
     <div>
-      <h1>Event Details</h1>
-      <p>Event ID is {params.eventId}</p>
+      <EventItem event={data.event}/>
       <p><Link to=".." relative="path">Back</Link></p>
     </div>
   );
 };
 
 export default EventDetailPage;
+
+export async function loader({request, params}) {
+  const id = params.eventId;
+
+// return fetch("http://localhost:8080/events/" + id);
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+
+    throw json({ message: "Could not find event details!"}, {status: 500})
+  } else {
+    return response;
+  }
+
+}
