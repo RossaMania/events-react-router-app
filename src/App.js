6e6a199edfa423fbda695f2f68397fbca9744516
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-23 20:43:09 -0500

    Created action func in NewEvent.js to post event data, with console log, if/else response block, and redirect to Events page. Imported to App.js with an action prop. Imported Form into EventForm.js, and added post method.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
@@ -24,22 +26,18 @@ const router = createBrowserRouter([
           { index: true, element: <EventsPage />, loader: eventsLoader },
           {
             path: ":eventId",
-            id: "event-detail",
-            loader: eventDetailLoader,
+            id: "event-detail", loader: eventDetailLoader,
             children: [
-              { index: true,
-              element: <EventDetailPage />, },
-              { path: "edit",
-              element: <EditEventPage /> },
+              { index: true, element: <EventDetailPage /> },
+              { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
   },
 ]);
-
 
 function App() {
   return <RouterProvider router={router} />;
