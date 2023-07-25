6d2657647e6ac627f1cbade1a078ea9b1f755be9
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-24 20:49:57 -0500

    Set up defer object in Events.js to return a loadevents() promise with an ‘events’ key. Destructured ‘events’ key to turn into a promise for resolve in Await. Returned loadedEvents as ‘events’ prop for EventsList in Await func. Imported Suspense from React and wrapped Await func with it. Applied fallback prop to Suspense with a ‘Loading…’ paragraph element, textAlign centered. Parsed data for response by making const resData, awaiting for response.json, and then return resData.events.

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,33 +1,38 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList.js";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-
-    // if (data.isError) {
-    //   return <p>{data.message}</p>
-    // }
-  const events = data.events;
-
-
+  const { events } = useLoaderData();
 
   return (
-    <div>
-      <EventsList events={events} />
-    </div>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
+
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events!" };
     // throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {status: 500,});
-    throw json({ message: "Could not fetch events!" }, {status: 500,});
+    throw json({ message: "Could not fetch events!" }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+
+}
+
+export function loader() {
+  return defer({
+events: loadEvents()
+})
 }
